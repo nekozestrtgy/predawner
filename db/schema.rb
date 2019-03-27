3c453dd0f4be99a6451a23fe5b8ed1eb51625722
nekozestrtgy <nekozestrtgy@gmail.com>
2019-03-28 00:03:25 +0900

    ✨ : Add geocode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_123423) do
+ActiveRecord::Schema.define(version: 2019_03_27_145456) do
 
   create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_03_25_123423) do
     t.string "homepage_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
