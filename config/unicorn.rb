daf7593cbb38851e7000d335e93878e897931cbf
nekozestrtgy <nekozestrtgy@gmail.com>
2019-04-21 18:19:29 +0900

    Fix unicorn.rb for unicorn error ver.2
    https://teratail.com/questions/58846

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,9 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/current"
+RAILS_ROOT = File.expand_path('../../', __FILE__)
+ENV['BUNDLE_GEMFILE'] = RAILS_ROOT + "/Gemfile"
+working_directory RAILS_ROOT
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
